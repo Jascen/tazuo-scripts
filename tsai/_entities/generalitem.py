dd2361360c6ae02c5e9a302d41c8a2ba191826c2
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-10-09 20:09:19 -0400

    - Add basic resource entities

---
+++ tsai/_entities/generalitem.py
@@ -0,0 +1,7 @@
+from models.generalitem import GeneralItem
+
+
+Board          = GeneralItem(7127, "board")
+Ingot          = GeneralItem(7154, "ingot")
+Leather        = GeneralItem(4225, "leather")
+Log            = GeneralItem(7136, "log")
