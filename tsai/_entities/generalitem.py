228f7e56dfb8520fcd9cf0cfdd69b61fe81083a5
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-10-09 20:21:49 -0400

    - More entities for LJ Radar

--- tsai/_entities/generalitem.py
+++ tsai/_entities/generalitem.py
@@ -1,7 +1,12 @@
-from models.generalitem import GeneralItem
+from tsai._models.generalitem import GeneralItem
 
 
+# Resources
 Board          = GeneralItem(7127, "board")
 Ingot          = GeneralItem(7154, "ingot")
 Leather        = GeneralItem(4225, "leather")
 Log            = GeneralItem(7136, "log")
+BarkFragment   = GeneralItem(12687, "bark fragment")
+
+# Tools
+Hatchet        = GeneralItem(3907, "hatchet")
