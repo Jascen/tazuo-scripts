fab5358e285f928f9944025043479a71ef175ae2
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-10-28 05:07:44 -0400

    - Title change - Clarify User Options at the bottom

--- tsai-compiled/gather/lumberjack-radar.py
+++ tsai-compiled/gather/lumberjack-radar.py
@@ -1,6 +1,9 @@
 """
-Name: Lumerjack Radar Assistant
-Description: Detects trees within 6 tiles and hues them
+Name: Tsai's Lumberjack Radar Assistant
+
+** USER CONFIGURABLE OPTIONS ARE AT THE BOTTOM **
+
+Description: Detects trees within 6 tiles and hues them.
   - Clicking the `Detect` button or `Q` hotkey will re-hue nearby trees and add them to the Radar
   - Clicking the button on the Radar will pathfind to and harvest the node
   - `E` hotkey will harvest the closest unharvested node
