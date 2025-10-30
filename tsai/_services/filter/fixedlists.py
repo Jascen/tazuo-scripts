8f0b46a7b5b001de7ebfe2285228b2d52d39c950
Devin Ulrey <Devin.Ulrey@gmail.com>
2025-10-30 19:30:06 -0400

    - Add core files for Organizer (Filter stuff)

---
+++ tsai/_services/filter/fixedlists.py
@@ -0,0 +1,33 @@
+from tsai._services.filter.implementations import AllFilter, AnyFilter, NameFilter, NotFilter, PropertyFilter, TypeFilter
+class FixedLists:
+    Instruments = AnyFilter([
+        TypeFilter(0xe9d),  # Tambourine
+        TypeFilter(0xe9e),  # Tambourine w/ Tassel
+        TypeFilter(0xeb3),  # Lute
+        TypeFilter(0xeb2),  # Harp
+        TypeFilter(0xe9c),  # Drum
+        TypeFilter(0x2805),  # Flute
+    ])
+    Slayers = AnyFilter([
+        PropertyFilter("Giant Killer"),
+        PropertyFilter("Supernatural Vanquishing"),
+        PropertyFilter("Weed Ruin"),
+        PropertyFilter("Serpentaur Execution"),
+        PropertyFilter("Orcish Demise"),
+        PropertyFilter("Ogre Extinction"),
+        PropertyFilter("Golem Destruction"),
+    ])
+    Jewelry = AnyFilter([
+        NameFilter("earring"),
+        NameFilter("beads"),
+        NameFilter("amulet"),
+        NameFilter("necklace"),
+        NameFilter("bracelet"),
+        AllFilter([  # Rings - The coloring of names provides False positives because "ring" is a substring of "string"
+            AnyFilter([
+                NameFilter(" ring"),
+                NameFilter("ring "),
+            ]),
+            NotFilter(PropertyFilter("requirement"))
+        ]),
+    ])
